cfe4c072f55649df4b95db0258bb902b667d997d
Thiago Felipe Ribeiro Brito <britoff02@gmail.com>
2026-02-13 17:41:05 -0300

    att

--- DESAFIOS_JS/WeDesing/js/scripts.js
+++ DESAFIOS_JS/WeDesing/js/scripts.js
@@ -18,10 +18,28 @@ var aboutUs = {
   "Valores": "<ul><li>Comprometimento</li><li>Inovação</li><li>Ética profissional</li><li>Superação dos resultados</li><li>Melhoria contínua</li></ul>"
 };
 
-var unseletected_color = "#646872";
-var seletected_color = "#2A2D34";
+var unselected_color = "#646872";
+var selected_color = "#2A2D34";
 
+single_tab = document.getElementsByClassName("single-tab")
 
+for (let i = 0 ; i < single_tab.length; i++){
+  single_tab[i].onclick = function (){
+    
+    for (let j = 0; j < single_tab.length; j++) {
+      single_tab[j].style.backgroundColor = unselected_color;
+      single_tab[j].style['font-weight'] = 'normal';
+    }
+
+    let selecionado = this.innerHTML
+
+    this.style.backgroundColor = selected_color
+    this.style['font-weight'] = "bold"
+    
+    document.getElementById("box-text").innerHTML = aboutUs[selecionado]
+
+  }
+}
 
 // Slider de serviços
 
@@ -42,6 +60,36 @@ var our_services = [
   }
   
 ];
+
+// "service-previous" 
+// "service-next"
+// "service-title"
+// "service-text"
+
+let servico_atual = 0
+
+document.getElementById("service-next").onclick = function () {
+  servico_atual++
+  if (servico_atual == 3){
+    servico_atual = 0
+  }
+  document.getElementById("service-title").innerHTML = our_services[servico_atual].title
+  document.getElementById("service-text").innerHTML = our_services[servico_atual].text
+
+}
+document.getElementById("service-previous").onclick = function () {
+  servico_atual--
+   if (servico_atual < 0){
+    servico_atual = 2
+  }
+  document.getElementById("service-title").innerHTML = our_services[servico_atual].title
+  document.getElementById("service-text").innerHTML = our_services[servico_atual].text
+
+}
+
+
+
+
 
 
 // Data Footer
